341022778a7b0e47ce4440878181b270b7516849
c0ughman <58052516+c0ughman@users.noreply.github.com>
2024-07-08 03:02:00 +0200

    Changed database

--- _1nbox_ai/settings.py
+++ _1nbox_ai/settings.py
@@ -92,10 +92,9 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
