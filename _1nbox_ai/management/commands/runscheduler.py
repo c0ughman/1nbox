3ba357aa308d4fcd5bd15993d23a55df5753585c
c0ughman <58052516+c0ughman@users.noreply.github.com>
2024-07-16 11:50:15 +0200

    Update runscheduler.py

--- _1nbox_ai/management/commands/runscheduler.py
+++ _1nbox_ai/management/commands/runscheduler.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 from _1nbox_ai.schedulers import timeLoop
+from django.utils import autoreload
 
 class Command(BaseCommand):
     help = 'Runs timeLoop'
 
     def handle(self, *args, **kwargs):
-        # Call your function here
-        timeLoop()
+        self.stdout.write('Starting background task...')
+        autoreload.run_with_reloader(timeLoop)
