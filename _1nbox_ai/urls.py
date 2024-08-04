fa66377b0ad2357a8eb5fdae0c198d45baf63eb1
c0ughman <58052516+c0ughman@users.noreply.github.com>
2024-08-04 16:00:00 +0200

    Added message_received

--- _1nbox_ai/urls.py
+++ _1nbox_ai/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('new_user/', views.new_user),
     path('user/<str:supabase_user_id>/', views.get_user_data),
     path('sign_up/', views.sign_up),
+    path('message_received/', views.message_received),
     path('new_settings/', views.new_settings),
     path('new_keywords/', views.new_keywords),
     path('redirect/', views.oauth_redirect, name='redirect'),
