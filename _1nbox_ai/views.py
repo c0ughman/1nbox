946c981a2211f908a9ccdc2e058f911d0d970ae3
c0ughman <58052516+c0ughman@users.noreply.github.com>
2024-08-27 17:05:56 +0200

    Fixin

--- _1nbox_ai/views.py
+++ _1nbox_ai/views.py
@@ -35,7 +35,7 @@ def create_topic(request):
 
         all_sources = sources + custom_rss
 
-        print (request)
+        print(request.text)
         
         if not name:
             return JsonResponse({'success': False, 'error': 'Topic name is required.'}, status=400)
