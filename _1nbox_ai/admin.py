28a1ee464e12d60f5af0e2e3a33d7b7ef73d803f
c0ughman <58052516+c0ughman@users.noreply.github.com>
2024-10-25 12:04:25 +0200

    Added Organization and Summary to admin view

--- _1nbox_ai/admin.py
+++ _1nbox_ai/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User, Topic
+from .models import Organization, User, Topic, Summary
 
+admin.site.register(Organization)
 admin.site.register(User)
 admin.site.register(Topic)
+admin.site.register(Summary)
