370d4e95ed89ab0ccf8b3cbee2f100a320217521
c0ughman <58052516+c0ughman@users.noreply.github.com>
2024-10-30 12:01:12 +0100

    Added other important imports

--- _1nbox_ai/apps.py
+++ _1nbox_ai/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 import firebase_admin
+from firebase_admin import credentials
+from django.conf import settings
 
 class MyAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
