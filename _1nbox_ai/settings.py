19a0d09cd3e556b525f72538f5ffaeaa272682bc
c0ughman <58052516+c0ughman@users.noreply.github.com>
2025-02-03 13:42:18 -0500

    Update settings.py

--- _1nbox_ai/settings.py
+++ _1nbox_ai/settings.py
@@ -96,8 +96,8 @@ TEMPLATES = [
 WSGI_APPLICATION = '_1nbox_ai.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
+    "https://1nbox-ai.com",
     "https://www.1nbox-ai.com",
-    "https://editor.weweb.io",
     "https://1nbox.netlify.app",
 ]
 
