51b874d4d91d165d6bac048655f3c33003f13566
c0ughman <58052516+c0ughman@users.noreply.github.com>
2025-04-28 13:48:35 +0200

    Update message.py

--- _1nbox_ai/message.py
+++ _1nbox_ai/message.py
@@ -73,7 +73,7 @@ def get_user_topics_summary(organization):
 
 def send_email(user, subject, content):
     message = Mail(
-        from_email='feed@trybriefed.com',
+        from_email=('feed@trybriefed.com', 'Briefed'),  # <-- this part
         to_emails=user.email,
         subject=subject,
         html_content=content
