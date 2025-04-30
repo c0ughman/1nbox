7a0249e09c78adcae2fa17bcd90a0492f04fecc6
c0ughman <58052516+c0ughman@users.noreply.github.com>
2025-04-30 13:22:16 +0200

    Update settings.py

--- _1nbox_ai/settings.py
+++ _1nbox_ai/settings.py
@@ -33,12 +33,16 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['app-1nbox-ai-fb8295a32cce.herokuapp.com/', '127.0.0.1',"https://nbox-ai-bb518.firebaseapp.com"]
+ALLOWED_HOSTS = ['app-1nbox-ai-fb8295a32cce.herokuapp.com', '127.0.0.1',"https://nbox-ai-bb518.firebaseapp.com"]
 
 SESSION_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"  # Blocks iframe embedding
 
 # FIREBASE
 FIREBASE_CREDENTIALS = {
